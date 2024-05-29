9a18336a99324ec073a0029c3a159ae8e5e4bec9
mfitrahrmd <tgcfitrah26@gmail.com>
2024-05-29 09:55:59 +0700

    add user param

--- app/src/main/java/com/mfitrahrmd/story/data/repositories/IAuthenticationRepository.kt
+++ app/src/main/java/com/mfitrahrmd/story/data/repositories/IAuthenticationRepository.kt
@@ -3,6 +3,6 @@ package com.mfitrahrmd.story.data.repositories
 import com.mfitrahrmd.story.data.entities.User
 
 interface IAuthenticationRepository {
-    suspend fun register(): Boolean
+    suspend fun register(user: User): Boolean
     suspend fun login(userAccount: User.Account): User.Account
 }
